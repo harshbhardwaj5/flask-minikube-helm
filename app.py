d3392c9be2ffe719cd22c0db22f5e08b9b65b3fa
harshbhardwaj5 <47530172+harshbhardwaj5@users.noreply.github.com>
2025-04-01 23:30:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "I am xwqhe bomb Hello securely from Flask on Minikube + Helm!"
+    return "I am xwqhe  Hello securely from Flask on Minikube + Helm!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
